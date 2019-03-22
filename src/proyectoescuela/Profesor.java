4e43d8eaf446e6da2d24d3a615a80e81476fa29e
Dulce <42220874+DulceAmaya@users.noreply.github.com>
2019-03-22 17:16:54 -0600

    Alumno y CarreraTecnica

---
+++ src/proyectoescuela/Profesor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoescuela;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dulf2
+ */
+public class Profesor {
+    
+    final String nombre;
+    final int id;
+    final int fechaDeNacimiento;
+    final String sexo;
+    String correo;
+    ArrayList<Materia> materiasImpartidas;
+    
+    
+    
+}
